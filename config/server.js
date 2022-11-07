eb5436113085f537685af2321b51fa18fc4a90a8
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-11-07 19:30:46 -0300

    modificando configuraciones del server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,9 @@
+// const strapi = require('@strapi/strapi');
+
+// strapi(/* {...} */).start();
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://campus-backend.vercel.app/'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
