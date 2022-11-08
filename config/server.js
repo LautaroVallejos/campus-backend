b8ca9ed25cf603a3d61b081ab93ea8da5b72b6f2
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-11-08 10:52:08 -0300

    cambios en el host server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '3.19.211.126'),
+  host: env('HOST', 'ec2-3-19-211-126.us-east-2.compute.amazonaws.com'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('URL', 'ec2-3-19-211-126.us-east-2.compute.amazonaws.com')
+  // url: env('URL', 'ec2-3-19-211-126.us-east-2.compute.amazonaws.com')
 });
