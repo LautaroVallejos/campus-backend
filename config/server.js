1281d44e1336ef9bfa4bb10dd6d9c77eda7659d8
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-11-08 10:16:42 -0300

    Configurando servidor

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '3.19.211.126'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('URL', 'ec2-3-19-211-126.us-east-2.compute.amazonaws.com')
 });
